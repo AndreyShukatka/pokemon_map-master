5ecb2d9252aad602d28e0e64d1b118bd345a8228
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-09-14 20:51:20 +0300

    Добавил перевод имён

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    image = models.ImageField(blank=True)
+    title_ru = models.CharField(max_length=200)
+    title_en = models.TextField(null=True)
+    title_ja = models.TextField(null=True)
+    image = models.ImageField(blank=True, default='None')
+    description = models.TextField(null=True)
     def __str__(self):
-        return self.title
+        return self.title_ru
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, null=False)
-    lat = models.FloatField()
-    lon = models.FloatField()
+    lat = models.FloatField(null=True)
+    lon = models.FloatField(null=True)
     appeared_at = models.DateTimeField(null=True)
     disappeared_at = models.DateTimeField(null=True)
     level = models.IntegerField(null=True)
@@ -17,3 +20,5 @@ class PokemonEntity(models.Model):
     strength = models.IntegerField(null=True)
     defence = models.IntegerField(null=True)
     stamina = models.IntegerField(null=True)
+    def __str__(self):
+        return self.pokemon.title
