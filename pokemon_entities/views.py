d4839dcb9efe29bec64d19dda8b484ef1c1c540a
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-09-14 16:52:09 +0300

    show pokemons on mainpage map.

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -3,7 +3,7 @@ import json
 
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
-from .models import Pokemon
+from .models import Pokemon, PokemonEntity
 
 
 MOSCOW_CENTER = [55.751244, 37.618423]
@@ -28,16 +28,13 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
-
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon in pokemons:
-        for pokemon_entity in pokemon['entities']:
-            add_pokemon(
-                folium_map, pokemon_entity['lat'],
-                pokemon_entity['lon'],
-                pokemon['img_url']
+    pokemon_entity = PokemonEntity.objects.all()
+    for pokemon in pokemon_entity:
+        add_pokemon(
+            folium_map, pokemon.lat,
+            pokemon.lon,
+            pokemon.pokemon.image.path
             )
 
     pokemons_on_page = []
